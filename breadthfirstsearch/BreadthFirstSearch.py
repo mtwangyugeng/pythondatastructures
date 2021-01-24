26639bbc13e3544daf66f19e92f874979b22b483
Simon Dickenson <mtwangyugeng@gmail.com>
2021-01-24 12:40:27 -0500

    BFS: doc

--- breadthfirstsearch/BreadthFirstSearch.py
+++ breadthfirstsearch/BreadthFirstSearch.py
@@ -4,7 +4,14 @@ class BFS():
 
     def bfs(self, s):
         '''
-        Runs breath-first search on self.graph.
+        Runs breath-first search on self.graph starting on node s. 
+        For each node v in self.graph, we record its distance
+        (aka. length of shortest path) from s to v.
+        Proof of Correctness: Base case the trivial, as when dist = 0
+        only node s has dist[s] = 0, rest is None.
+        To record the nodes with n + 1 for dist, we find the 
+        neighbours of the nodes with n for dist with dist as None.
+         
         @input s: the starting node in self.graph
         '''
         dist = dict.fromkeys(self.graph, None)
